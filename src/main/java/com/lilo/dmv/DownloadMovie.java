20db1b22207f95c3cc03f4f19be27e56b1ce3897
lilogia@gmail.com <lilogia@gmail.com@4e18bcda-4c00-dd5b-54d7-72f4b3f9069a>
2014-04-12 13:52:12 +0000

    1.1 version. Use one name for onlineLink, runnableFile, download File. Simpler the properties file

--- src/main/java/com/lilo/dmv/DownloadMovie.java
+++ src/main/java/com/lilo/dmv/DownloadMovie.java
@@ -1,4 +1,5 @@
 package com.lilo.dmv;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -14,6 +15,9 @@ import com.lilo.dmv.util.Handler;
 public class DownloadMovie {
 
 	public static void main(String[] args) {
+		String os = System.getProperty("os.name");
+		boolean isUnix = true;
+		if (os.startsWith("Windows")) isUnix = false;
 		
 		String inputProperties = args[0];
 		if (inputProperties == null || inputProperties.equals("")) {
@@ -35,54 +39,60 @@ public class DownloadMovie {
 		
 		String title = prop.getProperty("TITLE");
 		int type = Integer.parseInt(prop.getProperty("TYPE", "1"));
-		String inputFilename = prop.getProperty("INPUT_FILENAME");
-		String outputFilename = prop.getProperty("OUTPUT_FILENAME");
+
+		String theName = prop.getProperty("NAME", "nameit");
 		String savePath = prop.getProperty("SAVE_PATH");
+		
+		String onlineLinkFilename = savePath + File.separator + theName + "_online.txt";
+		String episodeSuperListFilename = savePath + File.separator + theName + "_superlist";
+		String runableFilename = savePath + File.separator + theName + "_run.bat";		
+		
 		int cmd = Integer.parseInt(prop.getProperty("CMD", "2"));
-		String localSaveFile = "";
-		if (cmd == 1) localSaveFile = prop.getProperty("FILENAME_PATTERN", "unknown");
+		
 		int startIndex = Integer.parseInt(prop.getProperty("START_INDEX", "1"));
 		int endIndex = Integer.parseInt(prop.getProperty("END_INDEX", "0")); // 0 = get to the last episode
 		
-		String os = System.getProperty("os.name");
-		boolean isUnix = true;
-		if (os.startsWith("Windows")) isUnix = false;
-		
-		System.out.println("Start to create Film info ... " + startIndex + "," + endIndex );
-
 		try {
 			Film film = new Film(title,type);
-			if (inputFilename != null && !inputFilename.equals("")) {
-				film.createEpisodesUseTuDouFile(inputFilename);
-			}else {
-				System.out.println("Search link of episodes: " + film.createEpisodesUseTuDouSearch());
-			}
 		
 			boolean flag = false;
+			String outputFile = "";
 			switch (cmd) {
 				case 1:					
 					System.out.println("- Create Runnable file ... ");
+					File inputFile = new File(onlineLinkFilename);
+					if (inputFile.exists()) {
+						film.createEpisodesUseTuDouFile(onlineLinkFilename);
+					}else {
+						System.out.println("You should run cmd=2 first to create online Link filename & check if they are correct.");
+						System.out.println("Anyway we will try to create from directly searching.");
+						System.out.println("Search link of episodes: " + film.createEpisodesUseTuDouSearch());
+					}
 					film.reduceEpisodeList(startIndex, endIndex);
-					flag = Handler.toRunnableFile(film, outputFilename, savePath, FilmPattern.DOWNLOAD_CMD_WGET, localSaveFile, isUnix);
+					flag = Handler.toRunnableFile(film, runableFilename, savePath, FilmPattern.DOWNLOAD_CMD_WGET, theName, isUnix);
+					outputFile = runableFilename;
 					System.out.println("Done.");
 					break;
 				case 2:
 					System.out.println("- Create Online link of episodes ... ");
-					flag = Handler.toEpisodeOnlineLinkFile(film, outputFilename);
+					film.createEpisodesUseTuDouSearch();
+					flag = Handler.toEpisodeOnlineLinkFile(film, onlineLinkFilename);
+					outputFile = onlineLinkFilename;
 					System.out.println("Done. Please re-check manually with the number of episodes to make sure the number of online links is appropriate with the number of episodes.");
 					break;
 				case 3:
 					System.out.println("- Create Download links of Episodes ... ");
 					film.reduceEpisodeList(startIndex, endIndex);
-					flag = Handler.toEpisodeDowndoadLinkFile(film, outputFilename);
+					flag = Handler.toEpisodeDowndoadLinkFile(film, episodeSuperListFilename);
+					outputFile = episodeSuperListFilename;
 					System.out.println("Done.");
 					break;
 			}
 			
 			if (flag) {
-				System.out.println("Successful create outputFile: " + outputFilename);
+				System.out.println("Successful create outputFile: " + outputFile);
 			}else {
-				System.out.println("Fail create outputFile: " + outputFilename);
+				System.out.println("Fail create outputFile: " + outputFile);
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
