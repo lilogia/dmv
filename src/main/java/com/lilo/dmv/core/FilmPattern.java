cb3bc2f9ff332576326bffab6a8f78c2d2097f98
lilogia@gmail.com <lilogia@gmail.com@4e18bcda-4c00-dd5b-54d7-72f4b3f9069a>
2014-08-17 09:51:54 +0000

    update to 1.2

--- src/main/java/com/lilo/dmv/core/FilmPattern.java
+++ src/main/java/com/lilo/dmv/core/FilmPattern.java
@@ -14,7 +14,15 @@ public class FilmPattern {
 	//FLVCD_SEARCH_PATTERN_SUPER
 	// http://www.flvcd.com/parse.php?kw=http%3A%2F%2Fv.youku.com%2Fv_show%2Fid_XNTkwNTEyNjUy.html&flag=one&format=super
 
-	final public static String FILE_TO_DOWNLOAD_PATTERN_FLVCD = "http://f.youku.com/player/getFlvPath/sid/00_00/st/flv/fileid/[0-9A-Z\\-]+\\?K=[0-9a-z]+"; //FLVCD_PATTERN
+	final public static String FILE_TO_DOWNLOAD_PATTERN_FLVCD 
+		= "http://k.youku.com/player/getFlvPath/sid/[0-9A-Za-z\\-]+_[0-9]+/st/flv/fileid/[0-9A-Z\\-]+\\?K=[0-9a-zA-Z;?=&]+"; //FLVCD_PATTERN
+		// before 17-Aug-2014 "http://f.youku.com/player/getFlvPath/sid/00_00/st/flv/fileid/[0-9A-Z\\-]+\\?K=[0-9a-z]+"; //FLVCD_PATTERN
+	
+	/* sample at 17-Aug-2014
+	 * http://k.youku.com/player/getFlvPath/sid/540785733914512002d6b_00/st/flv/fileid/0300020600537A2E6F3FE803B99CC8FBE8B76C-BDCB-3997-6ED1-0309E13BC40C?K=e9d57bc7fa65b5b3261dd850&amp;ctype=12&amp;ev=1&amp;oip=1887191773&amp;token=7394&amp;ep=dyaUHUGIUc0D7CLbjT8bZyzgIHFdXP4J9h%2BFg9JkALshS%2B%2FOm0vXsZ7GO%2FdCFYsQe1F1GJ2D3tiVbUZnYYI32hoQ3EehPvrmi4Ti5a1QxJl1ZxpEAcnXx1SZQDn1
+	 * http://k.youku.com/player/getFlvPath/sid/540785733914512002d6b_01/st/flv/fileid/0300020601537A2E6F3FE803B99CC8FBE8B76C-BDCB-3997-6ED1-0309E13BC40C?K=1cd2b076054fbcd82411e830&amp;ctype=12&amp;ev=1&amp;oip=1887191773&amp;token=7394&amp;ep=dyaUHUGIUc0D7CLbjT8bZyzgIHFdXP4J9h%2BFg9JkALohS%2B%2FOm0vXsZ7GO%2FdCFYsQe1F1GJ2D3tiVbUZnYYI32hoQ3EehPvrmi4Ti5a1QxJl1ZxpEAcnXx1SZQDn1
+	 */
+	
 	final public static String FILENAME_PATTERN = "%1$03d-%2$s_%3$02d.flv";	
 	final public static String FILENAME_PATTERN_SUFFIX = "%02d";
 	
